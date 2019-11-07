52a5f678913645cfce6f68ccfc01e78f07349de0
Patrick Crowe <pcrowe@hawk.iit.edu>
2019-11-07 15:52:37 -0600

    Made a control panel object

--- Analyze.js
+++ Analyze.js
@@ -1,5 +1,5 @@
 var myBits;
-var myBytes
+var myBytes;
 if (localStorage.getItem("myBits")>0){
 	myBits=localStorage.getItem("myBits");
 	myBytes=localStorage.getItem("myBytes");
@@ -12,29 +12,32 @@ document.getElementById("bytes").innerHTML = myBytes;
 if (myBits>=8) {
 	docuemnt.getElementById('bytes').style.display = "inline-block";
 }
-function analyze() {
-	localStorage.setItem('myBits', myBits++);
-	document.getElementById("bits").innerHTML = localStorage.getItem("myBits");
-}
-function restart() {
-	localStorage.setItem('myBits', 0);
-	localStorage.setItem('myBytes', 0);
-	myBits=0;
-	myBytes=0;
-	document.getElementById('bytes').style.display = "none";
-	document.getElementById("bits").innerHTML = localStorage.getItem("myBits");
-	document.getElementById("bytes").innerHTML = localStorage.getItem("myBytes");
-}
-function bitToByte() {
-	if (myBits>=8){
-		myBytes++;
-		myBits -=8;
-		localStorage.setItem('myBytes', myBytes);
-		localStorage.setItem('myBits', myBits);
+
+let ControlPanel = {
+	analyze: function() {
+		localStorage.setItem('myBits', myBits++);
+		document.getElementById("bits").innerHTML = localStorage.getItem("myBits");
+	},
+	restart: function() {
+		localStorage.setItem('myBits', 0);
+		localStorage.setItem('myBytes', 0);
+		myBits=0;
+		myBytes=0;
+		document.getElementById('bytes').style.display = "none";
+		document.getElementById("bits").innerHTML = localStorage.getItem("myBits");
 		document.getElementById("bytes").innerHTML = localStorage.getItem("myBytes");
-		document.getElementById("bits").innerHTHML = localStorage.getItem("myBits");
-	} else {
-		document.getElementById('errorWindow').innerHTML = "Not enough bits";
+	},
+	bitToByte: function() {
+		if (myBits>=8){
+			myBytes++;
+			myBits -=8;
+			localStorage.setItem('myBytes', myBytes);
+			localStorage.setItem('myBits', myBits);
+			document.getElementById("bytes").innerHTML = localStorage.getItem("myBytes");
+			document.getElementById("bits").innerHTHML = localStorage.getItem("myBits");
+		} else {
+			document.getElementById('errorWindow').innerHTML = "Not enough bits";
+		}
 	}
 }
 	
