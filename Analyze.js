09d90d618048a8e13157826256fa195bff3bd3fd
Patrick Crowe <pcrowe@hawk.iit.edu>
2019-11-07 13:11:40 -0600

    added if conditional to myBits initialization

--- Analyze.js
+++ Analyze.js
@@ -1,4 +1,7 @@
 var myBits = 0;
+if (localStorage.getItem("myBits")>0){
+	myBits=localStorage.getItem("myBits");
+}
 function analyze() {
 	localStorage.setItem('myBits', myBits++);
 	document.getElementById("bits").innerHTML = localStorage.getItem("myBits");
