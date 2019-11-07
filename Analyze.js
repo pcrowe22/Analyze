66a522bcc2c0aef13a24f82eb23c962cccfa1ab7
Patrick Crowe <pcrowe@hawk.iit.edu>
2019-11-07 13:26:02 -0600

    Added bytes

--- Analyze.js
+++ Analyze.js
@@ -1,13 +1,28 @@
 var myBits = 0;
+var myBytes = 0;
 if (localStorage.getItem("myBits")>0){
 	myBits=localStorage.getItem("myBits");
+	myBytes=localStorage.getItem("myBytes");
 }
 function analyze() {
 	localStorage.setItem('myBits', myBits++);
 	document.getElementById("bits").innerHTML = localStorage.getItem("myBits");
 }
-function reset() {
+function restart() {
 	localStorage.setItem('myBits', 0);
+	localStorage.setItem('myBytes', 0);
 	myBits=0;
+	myBytes=0;
 	document.getElementById("bits").innerHTML = localStorage.getItem("myBits");
 }
+function bitToByte() {
+	if (myBits>=8){
+		myBytes++;
+		myBits -=8;
+		localStorage.setItem('myBytes', myBytes);
+		localStorage.setItem('myBits', myBits);
+	} else {
+		document.getElementById('errorWindow', "Not enough bits");
+	}
+}
+	
