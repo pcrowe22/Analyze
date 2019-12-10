38a7fb747047e43655c7fa9b05bbfc3f3d563f4d
Patrick Crowe <pcrowe@hawk.iit.edu>
2019-12-10 17:05:17 -0600

    added myBits outside the display function

--- Analyze.js
+++ Analyze.js
@@ -1,6 +1,6 @@
 var myBits;
 var myBytes;
-if (localStorage.getItem("myBits")>0){
+if (localStorage.getItem("myBits")>=0){
 	myBits=localStorage.getItem("myBits");
 	myBytes=localStorage.getItem("myBytes");
 } else {
@@ -13,7 +13,8 @@ document.getElementById("bytes").innerHTML = myBytes;
 
 let ControlPanel = {
 	analyze: function() {
-		localStorage.setItem('myBits', myBits++);
+    myBits++;
+		localStorage.setItem('myBits', myBits);
 		document.getElementById("bits").innerHTML = localStorage.getItem("myBits");
 		if (myBits>=8) {
 			document.getElementById('bytes').style.display = "inline-block";
