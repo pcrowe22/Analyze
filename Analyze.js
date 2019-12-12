b9161ad1ea055db74596ad08a0b2e72cf224d6f0
Patrick Crowe <pcrowe@hawk.iit.edu>
2019-12-12 17:08:54 -0600

    Added hard reset and crash0 event

--- Analyze.js
+++ Analyze.js
@@ -1,15 +1,20 @@
 var myBits;
+var totalBits;
 var myBytes;
 var myBitLoopStrength;
 var bitInterval;
-if (localStorage.getItem("myBits")>=0){
+var checkpoints = { crash0: false};
+var myRestarts;
+if (localStorage.getItem("storTotalBits")>=0){
 	myBits=localStorage.getItem("myBits");
 	myBytes=localStorage.getItem("myBytes");
   myBitLoopStrength = localStorage.getItem("storBitLoopStrength");
+  myRestarts=localStorage.getItem("storTotalBits");
 } else {
 	myBits = 0;
 	myBytes = 0;
   myBitLoopStrength = 0;
+  myRestarts=0;
 }
 if (localStorage.getItem("storBitLoopStrength")>0){
   clearInterval(bitInterval);
@@ -18,15 +23,19 @@ if (localStorage.getItem("storBitLoopStrength")>0){
 document.getElementById("bits").innerHTML = myBits;
 document.getElementById("bytes").innerHTML = myBytes;
 document.getElementById("bitLoopStrength").innerHTML = myBitLoopStrength;
+//document.getElementById("restarts").innerHTML = myRestarts;
 
 let ControlPanel = {
 	analyze: function() {
     myBits++;
+    totalBits++;
 		localStorage.setItem('myBits', myBits);
+    localStorage.setItem('storTotalBits', totalBits);
 		document.getElementById("bits").innerHTML = localStorage.getItem("myBits");
 		if (myBits>=8) {
 			document.getElementById('bytes').style.display = "inline-block";
 		}
+    //document.getElementById("bitToByte").setAttribute("display", "block");
 	},
 	restart: function() {
 		localStorage.setItem('myBits', 0);
@@ -44,6 +53,11 @@ let ControlPanel = {
     document.getElementById("bitLoopStrength").innerHTML = localStorage.getItem("myBitLoopStrength");
   
     clearInterval(bitInterval);
+    
+    if (!checkpoints.crash0){
+      document.getElementById("errorWindwo").innerHTML = "Looks like you crashed. But you got a restart point!";
+      checkpoints.crash0 = true;
+    }
   },
 	bitToByte: function() {
 		if (myBits>=8){
@@ -70,13 +84,37 @@ let ControlPanel = {
 			document.getElementById("bytes").innerHTML = localStorage.getItem("myBytes");
       clearInterval(bitInterval);
       bitInterval = setInterval(changeBits, 10000/myBitLoopStrength);
+      if (myBitLoopStrength>3 && !checkpoints.crash0) {
+        setTimeout(systemCrash(), 10000);
+      }
     } else {
 			document.getElementById('errorWindow').innerHTML = "Not enough bytes";
 		}
-	}
+	},
+  showASCII: function() {
+    if (myBits>=256) {
+      myBits -=256;
+      localStorage.setItem("myBits", myBits);
+      document.getElementById("bits").innerHTML = localStorage.getItem("myBits");
+      //useASCII();
+    } else {
+      document.getElementById('errorWindow').innerHTML = "Not enough bits";
+    }
+  }
+  hardRestart: function() {
+    ControlPanel.restart();
+    totalBits=0;
+    localStorage.setItem("storTotalBits", 0);
+  }
 }
 function changeBits() {
   myBits++;
+  totalBits++;
   localStorage.setItem("myBits", myBits);
+  localStorage.setItem("storTotalBits", totalBits);
   document.getElementById("bits").innerHTML = localStorage.getItem("myBits");
+}
+function systemCrash() {
+  myRestarts++;
+  ControlPanel.restart();
 }
